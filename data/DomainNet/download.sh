986ae02904f8c51e4ebb508f7f673c6a2edf2a98
hibana2077 <hibana2077@gmail.com>
2026-03-20 08:17:16 +1000

    fix: unify dataset prefix for DomainNet text files

--- data/DomainNet/download.sh
+++ data/DomainNet/download.sh
@@ -47,17 +47,17 @@ done
 rm -f ./*.zip
 
 # Add the dataset prefix to the txt files
-sed -i 's#^#data/DomainNet/clipart/#' clipart_train.txt
-sed -i 's#^#data/DomainNet/clipart/#' clipart_test.txt
-sed -i 's#^#data/DomainNet/infograph/#' infograph_train.txt
-sed -i 's#^#data/DomainNet/infograph/#' infograph_test.txt
-sed -i 's#^#data/DomainNet/painting/#' painting_train.txt
-sed -i 's#^#data/DomainNet/painting/#' painting_test.txt
-sed -i 's#^#data/DomainNet/quickdraw/#' quickdraw_train.txt
-sed -i 's#^#data/DomainNet/quickdraw/#' quickdraw_test.txt
-sed -i 's#^#data/DomainNet/real/#' real_train.txt
-sed -i 's#^#data/DomainNet/real/#' real_test.txt
-sed -i 's#^#data/DomainNet/sketch/#' sketch_train.txt
-sed -i 's#^#data/DomainNet/sketch/#' sketch_test.txt
+sed -i 's#^#data/DomainNet/#' clipart_train.txt
+sed -i 's#^#data/DomainNet/#' clipart_test.txt
+sed -i 's#^#data/DomainNet/#' infograph_train.txt
+sed -i 's#^#data/DomainNet/#' infograph_test.txt
+sed -i 's#^#data/DomainNet/#' painting_train.txt
+sed -i 's#^#data/DomainNet/#' painting_test.txt
+sed -i 's#^#data/DomainNet/#' quickdraw_train.txt
+sed -i 's#^#data/DomainNet/#' quickdraw_test.txt
+sed -i 's#^#data/DomainNet/#' real_train.txt
+sed -i 's#^#data/DomainNet/#' real_test.txt
+sed -i 's#^#data/DomainNet/#' sketch_train.txt
+sed -i 's#^#data/DomainNet/#' sketch_test.txt
 
 echo "Done."
