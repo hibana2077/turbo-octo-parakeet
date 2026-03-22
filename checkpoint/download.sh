1a53a88f80b5f68c93b81db0e4a0668f4d6c8912
hibana2077 <hibana2077@gmail.com>
2026-03-22 18:34:00 +1000

    feat: Add download functionality for additional pretrained model in download.sh

--- checkpoint/download.sh
+++ checkpoint/download.sh
@@ -7,3 +7,8 @@ MODEL_URL="https://github.com/hibana2077/turbo-octo-parakeet/releases/download/v
 echo "Downloading pretrained model from ${MODEL_URL}..."
 curl -L -o imagenet21k_ViT-B_16.npz "${MODEL_URL}"
 echo "Download completed and saved to imagenet21k_ViT-B_16.npz"
+
+MODEL2_URL="https://github.com/hibana2077/turbo-octo-parakeet/releases/download/v1.0.0-pt/sam_ViT-B_16.npz"
+echo "Downloading pretrained model from ${MODEL2_URL}..."
+curl -L -o ViT-B_16.npz "${MODEL2_URL}"
+echo "Download completed and saved to ViT-B_16.npz"
