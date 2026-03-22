4cda388c64db0f301650f119a8bee1881bec74d5
hibana2077 <hibana2077@gmail.com>
2026-03-22 17:40:34 +1000

    refactor: Update download.sh to suppress output during extraction for cleaner execution

--- data/visda-2017/download.sh
+++ data/visda-2017/download.sh
@@ -1,11 +1,11 @@
 wget http://csr.bu.edu/ftp/visda17/clf/train.tar
-tar xvf train.tar
+tar -xf train.tar > /dev/null
 
 wget http://csr.bu.edu/ftp/visda17/clf/validation.tar
-tar xvf validation.tar  
+tar -xf validation.tar > /dev/null
 
 wget http://csr.bu.edu/ftp/visda17/clf/test.tar
-tar xvf test.tar
+tar -xf test.tar > /dev/null
 
 wget https://raw.githubusercontent.com/VisionLearningGroup/taskcv-2017-public/master/classification/data/image_list.txt
 
