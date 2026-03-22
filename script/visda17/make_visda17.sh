f95fb1988f29f7576457ba06fa7da280070891cb
hibana2077 <hibana2077@gmail.com>
2026-03-22 19:59:56 +1000

    feat: Update visda17 script to support multiple JFPD lambda values and increase num_steps to 4000

--- script/visda17/make_visda17.sh
+++ script/visda17/make_visda17.sh
@@ -7,13 +7,13 @@ OUT_FILE="${SCRIPT_DIR}/visda17.txt"
 FFTAT_FILE="${PROJECT_ROOT}/fftat_script.txt"
 
 # JFPD best params (ours)
-JFPD_LAMBDA=0.0001
+JFPD_LAMBDAS=(0.1 0.01 0.001 0.0001 0.00001)
 JFPD_ALPHA=0.5
 JFPD_MODE="jfpd"
 
 # Overrides requested by us
 TRAIN_BATCH_SIZE=64
-NUM_STEPS=2000
+NUM_STEPS=4000
 GPU_ID=0
 
 TASKS=("visda")
@@ -70,41 +70,43 @@ for BASE_NAME in "${TASKS[@]}"; do
   WARMUP_STEPS="$(extract_arg "$BASELINE_LINE" "warmup_steps")"
   OPTIMAL="$(extract_arg "$BASELINE_LINE" "optimal")"
 
-  LAMBDA_TAG="$(float_tag "$JFPD_LAMBDA")"
-  RUN_NAME="${BASE_NAME}_jfpdl${LAMBDA_TAG}_tb${TRAIN_BATCH_SIZE}_ns${NUM_STEPS}"
-  CMD="python3 main.py"
-  CMD+=" --dataset visda17"
-  CMD+=" --name ${RUN_NAME}"
-  CMD+=" --source_list ${SOURCE_LIST}"
-  CMD+=" --target_list ${TARGET_LIST}"
-  CMD+=" --test_list ${TEST_LIST}"
-  CMD+=" --num_classes ${NUM_CLASSES}"
-  CMD+=" --img_size ${IMG_SIZE}"
-  CMD+=" --train_batch_size ${TRAIN_BATCH_SIZE}"
-  CMD+=" --eval_batch_size ${EVAL_BATCH_SIZE}"
-  CMD+=" --num_steps ${NUM_STEPS}"
-  CMD+=" --warmup_steps ${WARMUP_STEPS}"
-  CMD+=" --learning_rate ${LEARNING_RATE}"
-  CMD+=" --gpu_id ${GPU_ID}"
-  CMD+=" --model_type ${MODEL_TYPE}"
-  CMD+=" --pretrained_dir ${PRETRAINED_DIR}"
-  CMD+=" --beta ${BETA}"
-  CMD+=" --gamma ${GAMMA}"
+  for JFPD_LAMBDA in "${JFPD_LAMBDAS[@]}"; do
+    LAMBDA_TAG="$(float_tag "$JFPD_LAMBDA")"
+    RUN_NAME="${BASE_NAME}_jfpdl${LAMBDA_TAG}_tb${TRAIN_BATCH_SIZE}_ns${NUM_STEPS}"
+    CMD="python3 main.py"
+    CMD+=" --dataset visda17"
+    CMD+=" --name ${RUN_NAME}"
+    CMD+=" --source_list ${SOURCE_LIST}"
+    CMD+=" --target_list ${TARGET_LIST}"
+    CMD+=" --test_list ${TEST_LIST}"
+    CMD+=" --num_classes ${NUM_CLASSES}"
+    CMD+=" --img_size ${IMG_SIZE}"
+    CMD+=" --train_batch_size ${TRAIN_BATCH_SIZE}"
+    CMD+=" --eval_batch_size ${EVAL_BATCH_SIZE}"
+    CMD+=" --num_steps ${NUM_STEPS}"
+    CMD+=" --warmup_steps ${WARMUP_STEPS}"
+    CMD+=" --learning_rate ${LEARNING_RATE}"
+    CMD+=" --gpu_id ${GPU_ID}"
+    CMD+=" --model_type ${MODEL_TYPE}"
+    CMD+=" --pretrained_dir ${PRETRAINED_DIR}"
+    CMD+=" --beta ${BETA}"
+    CMD+=" --gamma ${GAMMA}"
 
-  if [[ "$BASELINE_LINE" =~ (^|[[:space:]])--use_im([[:space:]]|$) ]]; then
-    CMD+=" --use_im"
-  fi
-  CMD+=" --theta ${THETA}"
-  if [[ "$BASELINE_LINE" =~ (^|[[:space:]])--use_cp([[:space:]]|$) ]]; then
-    CMD+=" --use_cp"
-  fi
-  CMD+=" --optimal ${OPTIMAL}"
-  CMD+=" --use_jfpd"
-  CMD+=" --jfpd_lambda ${JFPD_LAMBDA}"
-  CMD+=" --jfpd_alpha ${JFPD_ALPHA}"
-  CMD+=" --jfpd_mode ${JFPD_MODE}"
+    if [[ "$BASELINE_LINE" =~ (^|[[:space:]])--use_im([[:space:]]|$) ]]; then
+      CMD+=" --use_im"
+    fi
+    CMD+=" --theta ${THETA}"
+    if [[ "$BASELINE_LINE" =~ (^|[[:space:]])--use_cp([[:space:]]|$) ]]; then
+      CMD+=" --use_cp"
+    fi
+    CMD+=" --optimal ${OPTIMAL}"
+    CMD+=" --use_jfpd"
+    CMD+=" --jfpd_lambda ${JFPD_LAMBDA}"
+    CMD+=" --jfpd_alpha ${JFPD_ALPHA}"
+    CMD+=" --jfpd_mode ${JFPD_MODE}"
 
-  printf '%s\t%s\n' "$RUN_NAME" "$CMD" >> "$OUT_FILE"
+    printf '%s\t%s\n' "$RUN_NAME" "$CMD" >> "$OUT_FILE"
+  done
 done
 
 COUNT="$(awk 'NF && $1 !~ /^#/ {count++} END {print count+0}' "$OUT_FILE")"
